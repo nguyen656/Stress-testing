b31f74d4273a13731083a487dabb1eb92e70fdc2
Tran Nguyen Le <63175927+nguyen656@users.noreply.github.com>
2022-02-20 17:04:35 +0700

    Create brute.cpp

---
+++ brute.cpp
@@ -0,0 +1,18 @@
+/******************************************************
+
+    @file  : brute.cpp"
+    @author: nguyentrader"
+    @date  : 02/20/2022"
+
+*******************************************************/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    // Write your code here.
+    return 0;
+}
